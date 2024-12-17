e91f9667fb79fb6b994e9ab3d35c57c230622f12
Henrikas Varanauskas <henedronas@gmail.com>
2024-12-17 16:08:10 +0000

    store customers to file

--- FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/data/CustomerDataManager.java
+++ FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/data/CustomerDataManager.java
@@ -2,12 +2,15 @@ package bcu.cmp5332.bookingsystem.data;
 
 import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 import bcu.cmp5332.bookingsystem.model.Customer;
+import bcu.cmp5332.bookingsystem.model.Flight;
 import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class CustomerDataManager implements DataManager {
@@ -39,5 +42,13 @@ public class CustomerDataManager implements DataManager {
     @Override
     public void storeData(FlightBookingSystem fbs) throws IOException {
         // TODO: implementation here
+    	try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))) {
+            for (Customer customer : fbs.getCustomers()) {
+                out.print(customer.getID() + SEPARATOR);
+                out.print(customer.getName() + SEPARATOR);
+                out.print(customer.getPhone() + SEPARATOR);
+                out.println();
+            }
+        }
     }
 }
