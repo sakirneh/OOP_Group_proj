136f154f4563b37586ffc26fc0a2cd654aa0b5b2
Marius23a <tudoricamarius5@gmail.com>
2025-01-09 17:56:45 +0000

    Update FlightBookingSystemException.java

--- FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/main/FlightBookingSystemException.java
+++ FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/main/FlightBookingSystemException.java
@@ -1,5 +1,7 @@
 package bcu.cmp5332.bookingsystem.main;
 
+import java.io.IOException;
+
 /**
  * FlightBookingSystemException extends {@link Exception} class and is a custom exception
  * that is used to notify the user about errors or invalid commands.
@@ -10,4 +12,12 @@ public class FlightBookingSystemException extends Exception {
     public FlightBookingSystemException(String message) {
         super(message);
     }
+
+	public FlightBookingSystemException(String string, IOException e) {
+		
+	}
+
+	public FlightBookingSystemException(String string, NumberFormatException e) {
+	
+	}
 }
