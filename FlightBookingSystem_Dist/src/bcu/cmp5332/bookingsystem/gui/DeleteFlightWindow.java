e6f38cd74c2e3acc69e8b5980571e815e47a94a2
Marius23a <tudoricamarius5@gmail.com>
2025-01-09 18:36:27 +0000

    Update DeleteFlightWindow.java

--- FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/gui/DeleteFlightWindow.java
+++ FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/gui/DeleteFlightWindow.java
@@ -1,181 +1,93 @@
 package bcu.cmp5332.bookingsystem.gui;
 
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.IOException;
-import java.util.List;
-
-import javax.swing.JFrame;
-
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.UIManager;
-import javax.swing.table.DefaultTableModel;
-
-import bcu.cmp5332.bookingsystem.commands.Command;
 import bcu.cmp5332.bookingsystem.commands.RemoveFlight;
+import bcu.cmp5332.bookingsystem.gui.MainWindow;
 import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 import bcu.cmp5332.bookingsystem.model.Flight;
 import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 
-public class DeleteFlightWindow extends JFrame implements MouseListener{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.List;
 
-	private MainWindow mw;
-	private FlightBookingSystem fbs;
-	
-	 public DeleteFlightWindow(MainWindow mw,FlightBookingSystem fbs) {
-		 
-	        this.mw = mw;
-	        this.fbs = fbs;
-	        initialize();
-	    }
-	
-	 private void initialize() {
+public class DeleteFlightWindow extends JDialog implements ActionListener {
+    private FlightBookingSystem fbs;
+    private List<Flight> flightList;
+    private JTable flightTable;
+    private JButton deleteButton;
+    private JButton cancelButton;
 
-	        try {
-	            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	        } catch (Exception ex) {
+    public DeleteFlightWindow(MainWindow parent) {
+        super(parent, "Delete Flight", true);
 
-	        }
+        this.fbs = parent.getFlightBookingSystem();
+        this.flightList = fbs.getFlights();
 
-	        setTitle("Add a New Flight");
+        setLayout(new BorderLayout());
 
-	        setSize(350, 220);
-	        
+        // Create a table of flights
+        String[] columns = {"Flight No", "Origin", "Destination", "Departure Date"};
+        Object[][] data = new Object[flightList.size()][4];
+        for (int i = 0; i < flightList.size(); i++) {
+            Flight flight = flightList.get(i);
+            data[i][0] = flight.getFlightNumber();
+            data[i][1] = flight.getOrigin();
+            data[i][2] = flight.getDestination();
+            data[i][3] = flight.getDepartureDate();
+        }
 
-	        List<Flight> flightsList = fbs.getFlights();
-	        // headers for the table
-	        String[] columns = new String[]{"Flight No", "Origin", "Destination", "Departure Date"};
-	        int size = flightsList.size();
-	        Object[][] data = new Object[size][6];
-	        
-	        
-	        JTable table = new JTable();
-	        /*
-	        for (int i = 0; i < flightsList.size(); i++) {
-	        	Flight flight = flightsList.get(i);
-	        	data[i][0] = flight.getFlightNumber();
-	            data[i][1] = flight.getOrigin();
-	            data[i][2] = flight.getDestination();
-	            data[i][3] = flight.getDepartureDate();
-	            
-	        }
-	        */
-	        
-	        table.setModel( new DefaultTableModel(data,columns));;
-	        
-	        table.addMouseListener(new MouseListener() {
-	        
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					// TODO Auto-generated method stub
-					int row = table.rowAtPoint(e.getPoint());
-					System.out.println("This is the flights");
-					try {
-						
-						Command removeFlight = new RemoveFlight(row+1);
-						Object[][] temp = new Object[data.length -1][6];
-						
-						removeFlight.execute(fbs);
-						//dtmModel.removeRow(row );
-						
-						for(int i  = 0; i < row; i ++) {
-				        	temp[i][0] = data[i][0];
-				        	temp[i][1] = data[i][1];
-				        	temp[i][2] = data[i][2];
-				        	temp[i][3] = data[i][3];
-						}
-						
-						for(int i = row + 1; i < data.length; i ++) {
-							temp[i-1][0] = data[i][0];
-							temp[i-1][1] = data[i][1];
-							temp[i-1][2] = data[i][2];
-							temp[i-1][3] = data[i][3];
-						}
-						
-						
-						Object[][] data1 = new Object[][]{data};
-						data1 = temp;
-						table.setModel(new DefaultTableModel(data1,columns));
-						//mw.displayFlights();
-						//DefaultTableModel tblModel = (DefaultTableModel) table.getModel();
-						//tblModel.removeRow(row);
-					} catch (FlightBookingSystemException | IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					
-				}
+        flightTable = new JTable(data, columns);
+        flightTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-				@Override
-				public void mousePressed(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
+        JScrollPane scrollPane = new JScrollPane(flightTable);
+        add(scrollPane, BorderLayout.CENTER);
 
-				@Override
-				public void mouseReleased(MouseEvent e) {
-					// TODO Auto-generated method stub
-					//deleteFlight();
-				}
+        // Buttons for delete and cancel
+        JPanel buttonPanel = new JPanel();
+        deleteButton = new JButton("Delete");
+        cancelButton = new JButton("Cancel");
 
-				@Override
-				public void mouseEntered(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
+        buttonPanel.add(deleteButton);
+        buttonPanel.add(cancelButton);
 
-				@Override
-				public void mouseExited(MouseEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-	        	
-	        });
-	        
-	        
-	        setVisible(true);
-	        
-	        this.getContentPane().removeAll();
-	        this.getContentPane().add(new JScrollPane(table));
-	        this.revalidate();
-	    
+        add(buttonPanel, BorderLayout.SOUTH);
 
-	        
-	        setLocationRelativeTo(mw);
+        deleteButton.addActionListener(this);
+        cancelButton.addActionListener(this);
 
-	        setVisible(true);
+        setSize(600, 400);
+        setLocationRelativeTo(parent);
+        setVisible(true);
+        
+    }
 
-	    }
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == deleteButton) {
+            int selectedRow = flightTable.getSelectedRow();
+            if (selectedRow != -1) {
+                int flightID = selectedRow + 1; // +1 because flightID starts from 1, not 0
+                try {
+                    RemoveFlight removeFlight = new RemoveFlight(flightID);
+                    removeFlight.execute(fbs);
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+                    // Refresh the flight display in the main window
+                    ((MainWindow) getParent()).displayFlights();
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+                    JOptionPane.showMessageDialog(this, "Flight removed successfully.");
+                    dispose();
+                } catch (FlightBookingSystemException | IOException ex) {
+                    JOptionPane.showMessageDialog(this, "Error removing flight: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "Please select a flight to delete.", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+        } else if (e.getSource() == cancelButton) {
+            dispose();
+        }
+    }
 }
+
